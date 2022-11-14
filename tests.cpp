ae196be4b6cad8ce255a70fa4a27b52b8eb93ecd
Ashlee Tolentino <104875387+ashlee-tolentino@users.noreply.github.com>
2022-11-13 20:02:26 -0500

    made tests file

---
+++ tests.cpp
@@ -0,0 +1,12 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include "funcs.h"
+#include "coord3d.h"
+#include <cmath>
+
+TEST_CASE("Length"){
+  Coord3D point1 = {10, 20, 30};
+  Coord3D point2 = {0, 48, 152};
+  CHECK(length(&point1) == sqrt((10 * 10) + (20 * 20) + (30 * 30)));
+  CHECK(length(&point2) == sqrt((0 * 0) + (48 * 48) + (152 * 152)));
+}
